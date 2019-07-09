cf5e8ffb770e10cf6adbd661917de6828ba9d292
Josh Holder <jholder112233@gmail.com>
2019-07-09 16:21:35 +0100

    ternary in

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3"
 import * as d3Fetch from "d3-fetch"
 import * as d3Projections from "d3-geo-projection"
-import world from 'world-atlas/world/50m.json'
+import world from 'world-atlas/world/50m.json' 
 import * as topojson from 'topojson'
 import geoff from './geoff'
 import palette from "./palette"
@@ -353,3 +353,252 @@ Promise.all([d3Fetch.json("https://interactive.guim.co.uk/docsdata-test/1kO5_S91
     
     scrolly.watchScroll();
 });
+
+import * as chroma from "chroma-js"
+
+const drawTernary = () => {
+  const height = 860;
+const width = 860;
+
+const svg = d3.select(".interactive-wrapper").append("div")
+  .classed("svg-wrapper", true)
+  .style("height", height + "px")
+  .style("width", width + "px")
+  .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+function line([x1, y1], [x2, y2]) {
+    return function(t) {
+        return [
+        x1 + t * (x2 - x1),
+        y1 + t * (y2 - y1)
+        ]
+    }
+}
+  
+const chart = svg.append('g')
+    .attr("transform", `translate(${width / 2} ${height / 2})`)
+    .attr("font-family", "sans-serif");
+
+const size = Math.min(width, height) / 2;
+const yOffset = (size - Math.sin(30 * Math.PI / 180) * size) / 2;
+
+const [A, B, C] = [150, 30, -90].map(d => [
+    Math.cos(d * Math.PI / 180) * size,
+    Math.sin(d * Math.PI / 180) * size + yOffset
+]);
+
+const a = line(B, C);
+const b = line(C, A);
+const c = line(A, B);
+
+const labelOffset = 55;
+const [_A, _B, _C] = [150, 30, -90].map(d => [
+    Math.cos(d * Math.PI / 180) * (size + labelOffset),
+    Math.sin(d * Math.PI / 180) * (size + labelOffset) + yOffset
+]);
+
+const _a = line(_B, _C);
+const _b = line(_C, _A);
+const _c = line(_A, _B);
+
+const ticks = d3.range(0, 100, 50).concat(100)
+const grid = d3.range(0, 1, 0.5);
+
+// triangle
+chart.append("path")
+.attr("d", `M${A}L${B}L${C}Z`)
+// .attr("stroke", "#000")
+.attr("fill", "#f6f6f6")
+.style("shape-rendering", "crispEdges");
+
+// grid
+chart.append("g")
+.selectAll(".grid")
+.data([
+    grid.map(tick => [a(tick), b(1 - tick)]),
+    grid.map(tick => [b(tick), c(1 - tick)]),
+    grid.map(tick => [c(tick), a(1 - tick)])
+])
+.enter().append("g")
+    .selectAll(".gridlines")
+    .data(d => d)
+    .enter().append("line")
+    .attr("x1", d => d[0][0])
+    .attr("y1", d => d[0][1])
+    .attr("x2", d => d[1][0])
+    .attr("y2", d => d[1][1])
+    .attr("stroke", (d, i) => (i & 1) ? "#fff" : "#000")
+    .style("shape-rendering",  "auto")
+    .attr("stroke-width", (d, i) => (i & 1) ? 1 : 1);
+
+// ticks
+// chart.append("g")
+//     .attr("font-size", 10)
+//     .selectAll(".axis")
+//     .data([
+//         ticks.map(tick => ({ tick, pos: a(tick / 100), rot: 0, anchor: "start"})),
+//         ticks.map(tick => ({ tick, pos: b(tick / 100), rot: 60, anchor: "end"})),
+//         ticks.map(tick => ({ tick, pos: c(tick / 100), rot: -60, anchor: "end"}))
+//     ])
+//     .enter().append("g")
+//         .selectAll(".ticks")
+//         .data(d => d)
+//         .enter().append("text")
+//         .attr("transform", d => `translate(${d.pos}) rotate(${d.rot})`)
+//         .attr("text-anchor", d => d.anchor)
+//         .attr("dx", d => 10 * (d.anchor === "start" ? 1 : -1))
+//         .attr("dy", ".3em")
+//     .text(d => d.tick);
+
+// label
+chart.append("g")
+    .attr("font-size", 14)
+    .selectAll(".labels")
+    .data([
+        { label: "◀ Imported more from EU", pos: _a(0.51), rot: 60, "text-anchor": "end", "color": "#ffb500"},
+        { label: "◀ UK produces more", pos: _b(0.51), rot: -60, "text-anchor": "end", "color": "#c70000"},
+        { label: "◀ UK produces more", pos: _c(0.49), rot: 0, "text-anchor": "end", "color": "#c70000"},
+        { label: "Imported more from non-EU ▶", pos: _a(0.49), rot: 60, "text-anchor": "start", "color": "#00b2ff"},
+        { label: "Imported more from EU ▶", pos: _b(0.49), rot: -60, "text-anchor": "start", "color": "#ffb500"},
+        { label: "Imported more from non-EU ▶", pos: _c(0.51), rot: 0, "text-anchor": "start", "color": "#00b2ff"}
+    ])
+    .enter().append("text")
+        .attr("transform", d => `translate(${d.pos}) rotate(${d.rot})`)
+        .attr("text-anchor",d => d["text-anchor"])
+        .style("font-weight", "bold")
+        .style("fill", d => d.color)
+        .text(d => d.label)
+
+// data  
+
+const rScale = d3.scaleSqrt().domain([0, 14831450]).range([0, 50])
+
+const colourise = (d) => {
+  // console.log(d.importEU_perc);
+
+  if(Number(d.importEU_perc) > 0.5) {
+    return "#ffbb50"
+  }
+
+  if(Number(d.importNonEU_perc) > 0.5) {
+    return "#00b2ff"
+  }
+
+  if(Number(d.production_perc) > 0.5) {
+    return "#c70000"
+  }
+  return "#767676";
+}
+
+const positionLabel = ([x,y], labelPosition) => {
+  let newX = x;
+  let newY = y
+  let textAnchor = "middle";
+
+  if(labelPosition == "right") {
+    newX = newX + 10;
+    newY = newY + 4
+    textAnchor = "start"
+  }
+
+  if(labelPosition == "top") {
+    newX = newX;
+    newY = newY - 8
+  }
+
+  return [newX,newY, textAnchor]
+}
+
+const mouseenter = (d) => {
+  const ml = document.querySelector(".mouse-overlay");
+  ml.style.display = "block"
+  ml.style.left = (d.pos[0] + width/2) + "px";
+  ml.style.top = (d.pos[1] + height/2) + "px";
+
+  ml.querySelector(".bars-wrapper").innerHTML = `
+    <h4>${d.name}</h4>
+    <div class="bar-stack percent-uk" data-label="UK" data-perc="${Math.round(Number(d.data.production_perc)*100)}%" style="height: ${Number(d.data.production_perc)*100}px"></div>
+    <div class="bar-stack percent-eu" data-label="EU" data-perc="${Math.round(Number(d.data.importEU_perc)*100)}%" style="height: ${Number(d.data.importEU_perc)*100}px"></div>
+    <div class="bar-stack percent-non-eu" data-label="Non-EU" data-perc="${Math.round(Number(d.data.importNonEU_perc)*100)}%" style="height: ${Number(d.data.importNonEU_perc)*100}px"></div>
+  `;
+}
+
+d3Fetch.csv("<%= path %>/assets/sorted_2.csv").then(data => {
+  const foodsToLabel = [{name: "Meat, chicken", position: "right"}, {name: "Mushrooms and truffles", position: "right", displayName:  "Mushrooms"}, {name: "Tomatoes", position: "right"}, {name: "Spinach", position: "right"}, {name: "Honey, natural", position: "right"}, {name: "Potatoes", position: "right"}, {name: "Chillies and peppers, green", displayName: "Chillies and peppers", position: "right"}];
+  const cleanedData = data.map(d => ({
+      name: d.I,
+      color: colourise(d),
+      pos: [
+      A[0] * d.production_perc + B[0] * d.importNonEU_perc + C[0] * d.importEU_perc,
+      A[1] * d.production_perc + B[1] * d.importNonEU_perc + C[1] * d.importEU_perc
+      ],
+      total: d.total,
+      label: ((foodsToLabel.filter(c => d.I === c.name)).length > 0) ? (foodsToLabel.filter(c => d.I === c.name))[0] : undefined,
+      data: d
+  })); 
+
+  chart.selectAll(".countries")
+      .data(cleanedData.filter(d => !d.label))
+      .enter().append("circle")
+          // .attr("r", d => rScale(d.total))
+          .attr("r", d => 5)
+          .attr("id", d => d.name)
+          .attr("cx", d => d.pos[0])
+          .attr("cy", d => d.pos[1])
+          .attr("fill", d => chroma.mix(d.color, '#fff', 0.29))
+          .attr("stroke", d => {
+            // if(foodsToLabel.filter(c => d.name === c.name).length > 0) {
+            //   return "#000"
+            // }
+            return d.color
+          })
+          .on("mouseover", function(d) {
+            mouseenter(d);
+          })
+          .on("mouseleave", function(d) {
+            const ml = document.querySelector(".mouse-overlay");
+            ml.style.display = "none"
+          })
+          // .style("fill-opacity", "0.5")
+          // .append("title").text(d => d.name)
+
+    chart.selectAll(".countries")
+          .data(cleanedData.filter(d => d.label))
+          .enter().append("circle")
+              // .attr("r", d => rScale(d.total))
+              .attr("r", d => 5)
+              .attr("id", d => d.name)
+              .attr("cx", d => d.pos[0])
+              .attr("cy", d => d.pos[1])
+              .attr("fill", d => chroma.mix(d.color, '#fff', 0.29))
+              .attr("stroke", d => {
+                if(foodsToLabel.filter(c => d.name === c.name).length > 0) {
+                  return "#000"
+                }
+                return d.color
+              })
+              .on("mouseover", function(d) {
+                mouseenter(d);
+              })
+              .on("mouseleave", function(d) {
+                const ml = document.querySelector(".mouse-overlay");
+                ml.style.display = "none"
+              })
+
+  chart.selectAll("text.circle-labels")
+    .data(cleanedData.filter(d => d.label))
+    .enter()
+    .append("text")
+      .attr("x", d => (d.label) ? positionLabel(d.pos, d.label.position)[0] : d.pos[0])
+      .attr("y", d => (d.label) ? positionLabel(d.pos, d.label.position)[1] : d.pos[1])
+      .style("font-size", "13")
+      .style("fill", "#000")
+      .style("text-anchor", d => positionLabel(d.pos, d.label.position)[2])
+      .text(d => d.displayName || d.name)
+
+});
+}
+
+drawTernary();
